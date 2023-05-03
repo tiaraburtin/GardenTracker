7dd1e1fb98fa118f5290faf0a7ce53be1af118cd
Teli <7telita@gmail.com>
2023-05-03 16:55:34 -0500

    fixed delete button on current beds

--- Controllers/ZoneAPIController.cs
+++ Controllers/ZoneAPIController.cs
@@ -1,26 +1,37 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json.Linq;
-using Newtonsoft.Json;
-using System.Net.Http;
+﻿//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using Newtonsoft.Json.Linq;
+//using Newtonsoft.Json;
+//using System.Net.Http;
+//using Microsoft.AspNetCore.Mvc.Rendering;
 
-namespace Tracker.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ZoneAPIController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-        [HttpPost]
-        public async Task<IActionResult> GetZone(string zipCode)
-        {
-            return View();
-        }
-     }
+//namespace Tracker.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class ZoneAPIController : Controller
+//    {
+//        public async Task<IActionResult> Index()
+
+//        {
+//            //code from snippet
+//            //get access to this page
+//            //converts json to c#
+
+//            //return View(//get object from api and send it to view);
+//        }
+//        [HttpPost]
+//        public async Task<IActionResult> GetZone(string zipCode)
+//        {
+//            return View();
+//        }
+//     }
     
-}
+//}
 
+//create an object.
 
+//with the model get info from json
+//controller to mani info
+
+//make sure the object matches the fields in the model class
