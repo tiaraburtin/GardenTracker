4f69000e33e5918b209e9b95b46e2543ff3ffc08
ngohile <ngohile.yakubu@gmail.com>
2023-05-03 13:49:46 -0500

    updating see

--- ViewModels/AddSeedViewModel.cs
+++ ViewModels/AddSeedViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 using Tracker.Models;
 
 
@@ -7,14 +8,21 @@ namespace Tracker.ViewModels
     public class AddSeedViewModel
     {
         public int SeedId { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage =
+         "Name must be between 2 and 30 characters long")]
+        public string Name { get; set; }
+
         public int BedId { get; set; }
 
-        //public string? Name { get; set; }
         public Bed? Bed { get; set; }
 
-        //public DateTime DatePlanted { get; set; }
+        public Seed? Seed { get; set; }
 
-        //public string HardinessZone { get; set; }
+        [Required(ErrorMessage = "Date is required")]
+        public DateTime DatePlanted { get; set; }
+        [Required(ErrorMessage = "HardinessZone is required")]
+        public string HardinessZone { get; set; }
 
         public List<SelectListItem>? Seeds { get; set; }
 
