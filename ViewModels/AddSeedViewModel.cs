53162a0d5ed08455de2d5eeeff86d3e92efa1996
Tiara <tburtin1019@gmail.com>
2023-05-05 16:15:29 -0500

    updated waterschedule

--- ViewModels/AddSeedViewModel.cs
+++ ViewModels/AddSeedViewModel.cs
@@ -13,7 +13,7 @@ namespace Tracker.ViewModels
         public Bed? Bed { get; set; }
 
         public DateTime? DatePlanted { get; set; }
-        public int? WaterSchedule { get; set; }
+        public string? WaterSchedule { get; set; }
         public DateTime? NeedsWater { get; set; }
 
         public string HardinessZone { get; set; }
